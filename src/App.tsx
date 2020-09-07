cc8517420c503f269949cc3b509025009073ac5b
Gabriel Modesto <gabrielrmodesto5@gmail.com>
2020-09-07 20:03:14 -0300

    Components, pages and styles started

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import GlobalStyles from "./styles/GlobalStyles";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Profile from "./pages/Profile";
+import Repo from "./pages/Repo";
 
 function App() {
   return (
-    <h1>Hello world</h1>
+    <BrowserRouter>
+		<Header /> 
+		<Routes>
+			<Route path="/" element={<Profile />} />
+			<Route path="/:username" element={<Profile />} />
+			<Route path="/:username/:reponame" element={<Repo />} />
+		</Routes>
+		<Footer />
+
+		<GlobalStyles />
+	</BrowserRouter>
   );
 }
 
