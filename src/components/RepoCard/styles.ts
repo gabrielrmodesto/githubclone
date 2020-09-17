97ba3fc361b6c1a5b55942417c8495c3593fd5c5
Gabriel Modesto <gabrielrmodesto5@gmail.com>
2020-09-16 23:53:17 -0300

    Repo card with styled and icons

--- src/components/RepoCard/styles.ts
+++ src/components/RepoCard/styles.ts
@@ -1,6 +1,6 @@
-import styled, {css} from 'styled-components';
-import { RiBookmarkLine, RiStarLine } from 'react-icons/ri';
-import { AiOutlineFork } from 'react-icons/ai';
+import styled, { css } from "styled-components";
+import { RiBookMarkLine, RiStarLine } from "react-icons/ri";
+import { AiOutlineFork } from "react-icons/ai";
 
 export const Container = styled.div`
 	display: flex;
@@ -10,7 +10,65 @@ export const Container = styled.div`
 	border: 1px solid var(--border);
 	border-radius: 6px;
 `;
+export const Topside = styled.div`
+	> header {
+		display: flex;
+		align-items: center;
+		> a {
+			margin-left: 8px;
+			font-size: 14px;
+			font-weight: 600;
+			color: var(--link);
 
+			text-decoration: none;
+
+			&:focus,
+			&:hover {
+				text-decoration: underline;
+			}
+		}
+	}
+	> p {
+		margin: 8px 0 16px;
+		font-size: 12px;
+		color: var(--gray);
+		letter-spacing: 0.1px;
+	}
+`;
+export const Botside = styled.div`
+	> ul {
+		display: flex;
+		align-items: center;
+
+		> li {
+			display: flex;
+			align-items: center;
+			margin-right: 16px;
+
+			> span {
+				margin-left: 5px;
+				font-size: 12px;
+				color: var(--gray);
+			}
+		}
+	}
+	.language {
+		width: 12px;
+		height: 12px;
+		border-radius: 50%;
+		flex-shrink: 0;
+
+		&.other{
+			background: var(--other-language);
+		}
+		&.javascript{
+			background: var(--javascript);
+		}
+		&.typescript{
+			background: var(--typescript);
+		}
+	}
+`;
 const iconCSS = css`
 	width: 16px;
 	height: 16px;
@@ -18,7 +76,7 @@ const iconCSS = css`
 	flex-shrink: 0;
 `;
 
-export const RepoIcon = styled(RiBookmarkLine)`
+export const RepoIcon = styled(RiBookMarkLine)`
 	${iconCSS}
 `;
 export const StarIcon = styled(RiStarLine)`
@@ -27,5 +85,3 @@ export const StarIcon = styled(RiStarLine)`
 export const ForkIcon = styled(AiOutlineFork)`
 	${iconCSS}
 `;
-export const Topside = styled.div``;
-export const Botside = styled.div``;
