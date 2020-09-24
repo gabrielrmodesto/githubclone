5eeecc2c75afcf85ad2c68847645bdac9a201f7d
Gabriel Modesto <gabrielrmodesto5@gmail.com>
2020-09-23 23:36:13 -0300

    Username and reponame in repo page

--- src/pages/Repo/index.tsx
+++ src/pages/Repo/index.tsx
@@ -1,9 +1,49 @@
-import React from 'react';
-
-// import { Container } from './styles';
+import React from "react";
+import {Link} from "react-router-dom";
+import {
+	Container,
+	Breadcrumb,
+	RepoIcon,
+	Stats,
+	StarIcon,
+	ForkIcon,
+	LinkButton,
+	GithubIcon,
+} from "./styles";
 
 const Repo: React.FC = () => {
-  return <div />;
-}
+	return (
+		<Container>
+			<Breadcrumb>
+				<RepoIcon />
+				<Link className={"username"} to={"/gabrielrmodesto"}>
+					gabrielrmodesto
+				</Link>
+				<span>/</span>
+
+				<Link className={"reponame"} to={"/gabrielrmodesto/kaora"}>
+					Kaora
+				</Link>
+			</Breadcrumb>
+			<p>Projeto para ajudar pessoas</p>
+			<Stats>
+				<li>
+					<StarIcon />
+					<b>10</b>
+					<span>stars</span>
+				</li>
+				<li>
+					<ForkIcon />
+					<b>0</b>
+					<span>forks</span>
+				</li>
+			</Stats>
+			<LinkButton href={"https://github.com/gabrielrmodesto/kaora"}>
+				<GithubIcon />
+				<span>View on Github</span>
+			</LinkButton>
+		</Container>
+	);
+};
 
 export default Repo;
--- src/pages/Repo/styles.ts
+++ src/pages/Repo/styles.ts
@@ -1,5 +1,63 @@
-import styled from 'styled-components';
+import styled, { css } from "styled-components";
+import { RiBookMarkLine, RiStarLine } from "react-icons/ri";
+import { FaGithub } from "react-icons/fa";
+import { AiOutlineFork } from "react-icons/ai";
 
 export const Container = styled.div`
-  
+	display: flex;
+	flex-direction: column;
+	padding: 16px;
+	> p {
+		font-size: 16px;
+	}
+`;
+export const Breadcrumb = styled.div`
+	margin-bottom: 16px;
+	display: flex;
+	align-items: center;
+	flex-wrap: nowrap;
+	white-space: nowrap;
+	font-size: 18px;
+
+	> a {
+		color: var(--link);
+		text-decoration: none;
+
+		&:hover,
+		&:focus {
+			text-decoration: underline;
+		}
+		&.username {
+			margin-left: 8px;
+		}
+		&.reponame {
+			font-weight: 600;
+		}
+	}
+	> span {
+		padding: 0 5px;
+	}
+`;
+
+const iconCSS = css`
+	width: 16px;
+	height: 16px;
+	fill: var(--icon);
+	flex-shrink: 0;
+`;
+
+export const RepoIcon = styled(RiBookMarkLine)`
+	${iconCSS}
+`;
+
+export const Stats = styled.div``;
+export const StarIcon = styled(RiStarLine)`
+	${iconCSS}
+`;
+export const ForkIcon = styled(AiOutlineFork)`
+	${iconCSS}
+`;
+export const LinkButton = styled.a``;
+export const GithubIcon = styled(FaGithub)`
+	${iconCSS}
 `;
