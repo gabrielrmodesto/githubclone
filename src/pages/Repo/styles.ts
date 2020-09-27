ad0a455fe0fea97744e7d3aab75cb8b13a6f7bac
Gabriel Modesto <gabrielrmodesto5@gmail.com>
2020-09-27 19:35:00 -0300

    Styled repo page

--- src/pages/Repo/styles.ts
+++ src/pages/Repo/styles.ts
@@ -50,14 +50,46 @@ export const RepoIcon = styled(RiBookMarkLine)`
 	${iconCSS}
 `;
 
-export const Stats = styled.div``;
+export const Stats = styled.ul`
+	margin-top: 16px;
+	display: flex;
+	align-items: center;
+
+	> li {
+		display: flex;
+		align-items: center;
+		margin-right: 9px;
+
+		> * {
+			margin-right: 7px;
+			color: var(--gray);
+		}
+	}
+`;
 export const StarIcon = styled(RiStarLine)`
 	${iconCSS}
 `;
 export const ForkIcon = styled(AiOutlineFork)`
 	${iconCSS}
 `;
-export const LinkButton = styled.a``;
+export const LinkButton = styled.a`
+	text-decoration: none;
+	margin-top: 24px;
+	background: var(--gray-dark);
+	padding: 12px 17px;
+	border-radius: 24px;
+	width: max-content;
+	display: flex;
+	align-items: center;
+
+	> span {
+		color: var(--primary);
+	}
+	> svg {
+		fill: var(--primary);
+		margin-right: 10px;
+	}
+`;
 export const GithubIcon = styled(FaGithub)`
 	${iconCSS}
 `;
